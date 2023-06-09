5cd027188e2168fe6b8c21767832ad33676e25e5
sibilawrance <sibilawrance456@gmail.com>
2023-06-09 18:54:24 +0530

    React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <BrowserRouter>
+        <Header />
+        <Routes>
+        <Route path='/' Component={Home}/>
+        <Route path='/cart' Component={Cart}/>
+        </Routes>
+      
+      </BrowserRouter>
   );
 }
 
 export default App;
+ 
---
+++ src/Components/Home.js
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import SingleProduct from "./SingleProducts"
+import Filter from "./Filter"
+
+import { cartContext } from "../Context/Context";
+import Filters from "./Filter";
+
+const Home = () => {
+
+  const Cart = useContext(cartContext);
+  const {
+    state: { products },
+    productState: { sort, byStock, byFastDelivery, byRating, searchQuery },
+  } = Cart
+
+  const transformProducts = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter((prod) => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter((prod) => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(
+        (prod) => prod.ratings >= byRating
+      );
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.name.toLowerCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
+
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {transformProducts().map((prod) => (
+          <SingleProduct prod={prod} key={prod.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
